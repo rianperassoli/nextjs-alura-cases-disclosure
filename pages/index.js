4cb285aa03ef2da12097edf03589a61aaed2c00d
Rian Perassoli <rian.perassoli@gmail.com>
2022-11-16 09:09:34 -0300

    Created components and using images

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,3 @@
-import Link from "../src/components/Link"
+import HomeScreen from "../src/screen/HomeScreen";
 
-function Title({ children, as }) {
-  const Tag = as
-  return (
-    <>
-      <Tag>{children}</Tag>
-      <style jsx>{`
-        ${Tag} {
-          color:red;          
-        }
-      `}</style>
-    </>
-  )
-
-}
-
-export default function HomePage() {
-  return (
-    <div>
-      <Title as="h1">Alura Cases - Home</Title>
-      <Link href="/faq">Go to FAQ</Link>
-    </div>
-  )
-}
+export default HomeScreen
