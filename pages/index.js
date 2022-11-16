96549bf1af1575e9e12157e02ca511397269f6be
Rian Perassoli <rian.perassoli@gmail.com>
2022-11-16 08:43:58 -0300

    Creating style and global style in JS

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Link from "../src/components/Link"
 
+function Title({ children, as }) {
+  const Tag = as
+  return (
+    <>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color:red;          
+        }
+      `}</style>
+    </>
+  )
+
+}
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases - Home</h1>
+      <Title as="h1">Alura Cases - Home</Title>
       <Link href="/faq">Go to FAQ</Link>
     </div>
   )
